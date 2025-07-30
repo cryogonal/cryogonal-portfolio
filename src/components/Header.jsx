c4d9296dda612bd6f45750a1eac099e785948a29
Brandon Hui <huibrandon727@gmail.com>
2025-07-30 18:14:33 -0400

    added font + reformatted some things

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,18 +39,18 @@ function Header() {
                             <div className="panel-content">
                                 {activeTab === 'experience' && (
                                 <>
-                                    <h2>experience</h2>
-                                    <p>—— Software Engineer for RAS@Pitt ——</p>
-                                    <p>Developing and simulating a rover with ROS 2 Humble, C++ and Python.</p>
+                                    <h1>experience</h1>
+                                    <p>———— <b>Software Engineer for RAS@Pitt</b> ————</p>
+                                    <p>Developed and simulated a rover with ROS 2 Humble, C++ and Python.</p>
                                     <p>Collaborated with mechanical and electrical engineers to integrate software with hardware.</p>
-                                    <p>—— Software Developer for ScottyLabs @ CMU ——</p>
+                                    <p>———— <b>Software Developer for ScottyLabs @ CMU</b> ————</p>
                                     <p>Developing a finance portal utilizing Python, Flask, NodeJS, Puppeteer, and Redis for backend, React for frontend, and PostgreSQL for the database.</p>
                                     <p>Working with peers from both Pitt and CMU to develop and test software.</p>
                                 </>
                                 )}
                                 {activeTab === 'about' && (
                                 <>
-                                    <h2>about me</h2>
+                                    <h1>about me</h1>
                                     <p>Hey! I'm Brandon Hui, a junior at the University of Pittsburgh studying computer science and philosophy. I dream of working in software, focusing on backend development, IT, and AI.</p>
                                     <p>Outside of programming, I love to play video games, discover new music, and hang out with friends! I've recently been addicted to Terraria and am currently trying to complete the Calamity mod.</p>
                                     <p>Hope you enjoy looking around!</p>
@@ -58,7 +58,7 @@ function Header() {
                                 )}
                                 {activeTab === 'contact' && (
                                 <>
-                                    <h2>contact me</h2>
+                                    <h1>contact me</h1>
                                     <p>
                                     <a href="mailto:huibrandon727@gmail.com" className="email-link">
                                         my email!
